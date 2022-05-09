3d4169e02e38906729225b8a25819186ccaf2f6a
Sheep-realms <38580457+sheep-realms@users.noreply.github.com>
2022-05-09 16:40:34 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,140 @@
+$(document).ready(function(){
+    let $page = $('#content');
+    $page.html('');
+    let miclmt = 0;
+    let micnew = false;
+    let index = 0
+    mdata.forEach(obj => {
+        if (obj.type == 'h3') {
+            micnew = true;
+            $page.append('<h3>' + obj.title + '</h3>');
+        } else if (obj.type == 'music') {
+            if (micnew) {
+                micnew = false;
+                miclmt++;
+                $page.append('<div class="music-list m' + miclmt + '"></div>');
+            }
+            $('#content .music-list.m' + miclmt).append(`
+                <div class="music-box" data-index="` + index + `">
+                    <div class="music-icon" style="--img:url(../res/img/music_icon/`
+                        + ( obj?.icon ? obj?.icon : 'unknow.png' )
+                        + `)"></div>
+                    <div class="music-info">
+                        <span class="title">` + obj.title + `</span>
+                        <span class="author">` + (obj?.author ? obj?.author : '') + `</span>
+                        <span class="from">` + (obj?.from ? obj?.from : '') + `</span>
+                    </div>
+                </div>
+            `);
+        }
+        index++;
+    });
+});
+
+$('#content').on('click', '.music-box', function() {
+    $('#side').removeClass('hide');
+    $('.music-box.selected').removeClass('selected');
+    $(this).addClass('selected');
+
+    let obj = mdata[$(this).data('index')];
+    if (obj?.icon) {
+        $('#side .icon').attr('style', '--img:url(../res/img/music_icon/' + obj?.icon + ')');
+    } else {
+        $('#side .icon').attr('style', '--img:url(../res/img/music_icon/unknow.png)');
+    }
+    $('#side .title').text(obj.title);
+    $('#side .author').text(obj?.author ? obj?.author : '（未知）' );
+    $('#side .from').text(obj?.from ? obj?.from : '（未知）' );
+
+    $('#side .use').text('');
+    if (obj?.use) {
+        obj.use.forEach(obj2 => {
+            $('#side .use').append('<a class="tag" href="'
+            + biliLink(obj2.bvid, obj2?.pid, obj2?.time)
+            + '" target="_blank">' + obj2.bvid
+            + (obj2?.pid ? ' P' + obj2?.pid : '')
+            + (obj2?.time != undefined ? ' ' + setTime(obj2?.time) : '') + '</a>');
+        });
+    }
+
+    $('#side .play').text('');
+    if (obj?.play) {
+        if (obj.play?.m163) {
+            $('#side .play').append('<a class="tag" href="https://music.163.com/#/song?id=' + obj.play.m163 + '" target="_blank">网易云音乐</a>');
+        }
+        if (obj.play?.bili) {
+            $('#side .play').append('<a class="tag" href="'
+            + biliLink(obj.play.bili.bvid, obj.play.bili?.pid, obj.play.bili?.time)
+            + '" target="_blank">Bilibili</a>');
+        }
+        if (obj.play?.nico) {
+            $('#side .play').append('<a class="tag" href="'
+            + nicoLink(obj.play.nico.smid)
+            + '" target="_blank">Niconico</a>');
+        }
+    } else {
+        $('#side .play').text('（暂无资源）');
+    }
+});
+
+$('#side').on('click', '.close', function() {
+    $('#side').addClass('hide');
+});
+
+$(document).mousemove(function(e){
+    $('.bg .h').css('left', e.pageX);
+    $('.bg .w').css('top', e.pageY);
+    $('.bg .b').css('top', e.pageY - 16);
+    $('.bg .b').css('left', e.pageX - 16);
+});
+
+$(document).mousedown(function(){
+    $('.bg').addClass('active');
+});
+
+$(document).mouseup(function(){
+    $('.bg').removeClass('active');
+});
+
+function biliLink(video, pid=undefined, time=undefined) {
+    let str = "https://www.bilibili.com/video/" + video;
+
+    if(pid || time) {
+        str += "?"
+        let k = false;
+        if(pid) {
+            str += ("p=" + pid);
+            k = true;
+        }
+        if(time) {
+            str += ((k ? "&" : "") + "t=" + time);
+        }
+    }
+
+    return str;
+}
+
+function nicoLink(smid) {
+    return "https://www.nicovideo.jp/watch/sm" + smid;;
+}
+
+
+function setTime(value){
+	let s = value;
+    let m = 0;
+    let h = 0;
+    h = Math.floor(s / 3600);
+    s -= h * 3600;
+    m = Math.floor(s / 60);
+    s -= m * 60;
+    return (h != 0 ? h + ':' : '')
+        + (h != 0 ? zero(m) : m)
+        + ':' + zero(s);
+}
+
+function zero(value) {
+    if (value < 10) {
+        return "0" + value;
+    }
+    return value;
+}
---
+++ js/data.js
@@ -0,0 +1,703 @@
+let template = [
+    {
+        type: "music",
+        title: "回レ！雪月花",
+        author: "原田ひとみ / 茅野愛衣 / 小倉唯",
+        from: "回レ！雪月花",
+        icon: "163/28018274.jpg",
+        play: {
+            m163: "28018274"
+        },
+        use: [
+            {
+                bvid: "BV1554y1d7Hh",
+                time: 52
+            }, {
+                bvid: "BV1QE411Q7kk",
+                time: 92
+            }
+        ]
+    }
+]
+
+let mdata = [
+    {
+        type: "h3",
+        title: "2022年"
+    }, {
+        type: "music",
+        title: "もってけ！セーラーふく",
+        author: "平野綾 / 加藤英美里 / 福原香織 / 遠藤綾",
+        from: "TVアニメ『らき☆すた』オープニングテーマ もってけ! セーラーふく",
+        icon: "163/28892268.png",
+        play: {
+            m163: "28892268",
+            bili: {
+                bvid: "BV1a5411w7Hs"
+            }
+        },
+        use: [
+            {
+                bvid: "BV1y34y1r7iJ"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Literature",
+        author: "上田丽奈",
+        from: "リテラチュア",
+        icon: "163/1484844664.jpg",
+        play: {
+            m163: "1484844664",
+            bili: {
+                bvid: "BV1pz4y1o7zz",
+                pid: 1
+            }
+        },
+        use: [
+            {
+                bvid: "BV1w44y1s7B2"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "春节序曲",
+        author: "中国国家交响乐团",
+        from: "金色中国",
+        icon: "163/396696.jpg",
+        play: {
+            m163: "396696"
+        },
+        use: [
+            {
+                bvid: "BV1vS4y1Z7Rk"
+            }
+        ]
+    }, {
+        type: "h3",
+        title: "2021年"
+    }, {
+        type: "music",
+        title: "The Arcade",
+        author: "Machinimasound",
+        from: "Machinimasound, Vol. 2",
+        icon: "163/1480885418.jpg",
+        play: {
+            m163: "1480885418"
+        },
+        use: [
+            {
+                bvid: "BV1LF411i7LQ"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Dj Update Status Baru",
+        author: "Adit Sparky",
+        from: "Dj Update Status Baru",
+        icon: "163/1858889825.jpg",
+        play: {
+            m163: "1858889825"
+        },
+        use: [
+            {
+                bvid: "BV1ru411Z7Hz"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "山魔王的大厅",
+        author: "爱德华·格里格",
+        from: "皮尔金第一组曲（曲号46）",
+        icon: "163/1459803926.jpg",
+        play: {
+            m163: "1459803926"
+        },
+        use: [
+            {
+                bvid: "BV16U4y1L79g"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Monkey Spinning Moneys",
+        author: "Kevin MacLeod",
+        use: [
+            {
+                bvid: "BV1cV411n77h",
+                time: 0
+            }, {
+                bvid: "BV1jv411Y7ju",
+                time: 0
+            }, {
+                bvid: "BV17K4y1H74e",
+                time: 0
+            }
+        ]
+    }, {
+        type: "music",
+        title: "RAGE OF DUST",
+        author: "SPYAIR",
+        from: "RAGE OF DUST (初回限定盤)",
+        icon: "163/442589034.jpg",
+        play: {
+            m163: "442589034"
+        },
+        use: [
+            {
+                bvid: "BV1cV411n77h",
+                time: 32
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Cyber Prisoner",
+        from: "ヘタ鬼HetaOni M U S I C",
+        icon: "163/399366350.jpg",
+        play: {
+            m163: "399366350"
+        },
+        use: [
+            {
+                bvid: "BV1cV411n77h",
+                time: 81
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Show Me The Money",
+        author: "吴俊城",
+        from: "풀하우스 테이크 2 OST",
+        icon: "163/25717059.jpg",
+        play: {
+            m163: "25717059"
+        },
+        use: [
+            {
+                bvid: "BV1jv411Y7ju",
+                time: 20
+            }, {
+                bvid: "BV17K4y1H74e",
+                time: 76
+            }
+        ]
+    }, {
+        type: "music",
+        title: "DEJA VU",
+        author: "Dave Rodgers",
+        from: "TV动画《头文字D》第二季第13集插曲",
+        icon: "163/1364973839.jpg",
+        play: {
+            m163: "1364973839"
+        },
+        use: [
+            {
+                bvid: "BV1jv411Y7ju",
+                time: 31
+            }
+        ]
+    }, {
+        type: "music",
+        title: "夜的钢琴曲五",
+        author: "邓壬鑫",
+        from: "夜の钢琴",
+        icon: "163/558261233.jpg",
+        play: {
+            m163: "558261233"
+        },
+        use: [
+            {
+                bvid: "BV1jv411Y7ju",
+                time: 66
+            }
+        ]
+    }, {
+        type: "music",
+        title: "猪突猛進",
+        author: "百石元",
+        from: "冴えない彼女の育てかた 第2巻 特典CD",
+        icon: "163/31246600.jpg",
+        play: {
+            m163: "31246600"
+        },
+        use: [
+            {
+                bvid: "BV1jv411Y7ju",
+                time: 202
+            }
+        ]
+    }, {
+        type: "music",
+        title: "MOM",
+        author: "蜡笔小心",
+        from: "MOM",
+        icon: "163/1387581250.jpg",
+        play: {
+            m163: "1387581250"
+        },
+        use: [
+            {
+                bvid: "BV17K4y1H74e",
+                time: 132
+            }
+        ]
+    }, {
+        type: "h3",
+        title: "2020年"
+    }, {
+        type: "music",
+        title: "魔理沙は大変なものを盗んでいきました",
+        author: "IOSYS",
+        from: "東方乙女囃子",
+        icon: "163/749818.jpg",
+        play: {
+            bili: {
+                bvid: "BV1is411w7fb"
+            }
+        },
+        use: [
+            {
+                bvid: "BV1BC4y1b7Xc"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Regurgitation Pumping Station",
+        author: "Kyle Gabler",
+        from: "World of Goo Soundtrack",
+        icon: "163/1639858.jpg",
+        play: {
+            m163: "1639858"
+        },
+        use: [
+            {
+                bvid: "BV1Wz411v7e9"
+            }, {
+                bvid: "BV1364y1u7pL"
+            }, {
+                bvid: "BV1zE411T7jR",
+                pid: 2
+            }
+        ]
+    }, {
+        type: "music",
+        title: "escape",
+        author: "xhloe",
+        from: "CrackleCradle",
+        play: {
+            bili: {
+                bvid: "BV1zb411K7TL",
+                pid: 23
+            }
+        },
+        use: [
+            {
+                bvid: "BV1o54y1i7RE"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "ぶっぶぶっぶぶーだね、らき☆すた",
+        author: "神前暁",
+        from: "らき☆すた BGM&ラジオ番組「らっきー☆ちゃんねる」のダイジェストを収録したスペシャルCD6",
+        icon: "163/462328.jpg",
+        play: {
+            m163: "462328"
+        },
+        use: [
+            {
+                bvid: "BV1o5411W76Z",
+                time: 0
+            }
+        ]
+    }, {
+        type: "music",
+        title: 'The Benny Hill Show ("Yakety Sax")',
+        author: "Boots Randolph",
+        from: "Television's Greatest Hits, Vol.6: Remote Control",
+        icon: "163/26935244.jpg",
+        play: {
+            m163: "26935244"
+        },
+        use: [
+            {
+                bvid: "BV1o5411W76Z",
+                time: 28
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Funky Stars",
+        author: "BQuazar",
+        from: "Funky Stars",
+        icon: "163/428350808.jpg",
+        play: {
+            m163: "428350808"
+        },
+        use: [
+            {
+                bvid: "BV1RC4y1a7Le"
+            }, {
+                bvid: "BV1zE411T7jR",
+                pid: 1
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Astronomia",
+        author: "Vicetone / Tony Igy",
+        from: "Astronomia",
+        icon: "163/430114655.jpg",
+        play: {
+            m163: "430114655"
+        },
+        use: [
+            {
+                bvid: "BV1854y1X7Lh",
+                time: 87
+            }
+        ]
+    }, 
+    template[0],
+    {
+        type: "music",
+        title: "スキップスキップだよ、らき☆すた",
+        author: "神前暁",
+        from: "らき☆すた BGM&ラジオ番組「らっきー☆ちゃんねる」のダイジェストを収録したスペシャルCD5",
+        icon: "163/462288.jpg",
+        play: {
+            m163: "462288"
+        },
+        use: [
+            {
+                bvid: "BV1zK4y1C7EP",
+                time: 0
+            }
+        ]
+    }, {
+        type: "music",
+        title: "ファイルナファタンジー",
+        author: "神前暁",
+        from: "らき☆すた BGM&ラジオ番組「らっきー☆ちゃんねる」のダイジェストを収録したスペシャルCD5",
+        icon: "163/462288.jpg",
+        play: {
+            m163: "462295"
+        },
+        use: [
+            {
+                bvid: "BV1zK4y1C7EP",
+                time: 42
+            }
+        ]
+    }, {
+        type: "music",
+        title: "执棋者之骨",
+        author: "塞壬唱片-MSR / BaoUner",
+        from: "明日方舟OST1",
+        icon: "163/1371760674.jpg",
+        play: {
+            m163: "1371760674"
+        },
+        use: [
+            {
+                bvid: "BV1zK4y1C7EP",
+                time: 53
+            }, {
+                bvid: "BV1Z7411V7va"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Theme of Ken",
+        author: "青木征洋",
+        from: "Street Fighter V Original Soundtrack",
+        icon: "163/406096264.jpg",
+        play: {
+            m163: "406096264"
+        },
+        use: [
+            {
+                bvid: "BV1zK4y1C7EP",
+                time: 92
+            }
+        ]
+    }, {
+        type: "music",
+        title: "MKAlieZ",
+        author: "小林未郁",
+        from: "アルドノア・ゼロ オリジナル・サウンドトラック",
+        icon: "163/29307198.jpg",
+        play: {
+            m163: "29307198"
+        },
+        use: [
+            {
+                bvid: "BV1At4y1U7jr"
+            }
+        ]
+    }, {
+        type: "music",
+        title: "新宝島",
+        author: "サカナクション",
+        from: "834.194",
+        icon: "163/1372726248.jpg",
+        play: {
+            m163: "1372726248",
+            bili: {
+                bvid: "BV1EV411s7vu"
+            }
+        },
+        use: [
+            {
+                bvid: "BV12E411c725",
+                time: 3
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Freesia",
+        author: "Uru",
+        from: "フリージア",
+        icon: "moegirl/Fressia.jpg",
+        play: {
+            bili: {
+                bvid: "BV1vt41117tQ",
+                pid: 8
+            }
+        },
+        use: [
+            {
+                bvid: "BV12E411c725",
+                time: 26
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Nyanyanyanyanyanyanya!",
+        author: "初音ミク / daniwell",
+        icon: "moegirl/Nyanyanyanyanyanyanya.jpg",
+        play: {
+            nico: {
+                smid: "11509720"
+            },
+            bili: {
+                bvid: "BV1Hx411c7N6"
+            }
+        },
+        use: [
+            {
+                bvid: "BV12E411c725",
+                time: 41
+            }
+        ]
+    }, {
+        type: "music",
+        title: "battle/116_1465.mp3",
+        from: "舰队Collection",
+        use: [
+            {
+                bvid: "BV1gE411A7ny"
+            }
+        ]
+    }, {
+        type: "h3",
+        title: "2019年"
+    }, {
+        type: "music",
+        title: "Revenge",
+        author: "CaptainSparklez",
+        from: "Revenge",
+        icon: "163/1389794615.jpg",
+        play: {
+            m163: "1389794615"
+        },
+        use: [
+            {
+                bvid: "BV1LE411X74u",
+                time: 0
+            }, {
+                bvid: "BV1QE411Q7kk",
+                time: 0
+            }
+        ]
+    }, {
+        type: "music",
+        title: "恋愛サーキュレーション",
+        author: "花澤香菜",
+        from: "恋愛サーキュレーション & あとがたり",
+        icon: "163/579954.jpg",
+        play: {
+            m163: "579954"
+        },
+        use: [
+            {
+                bvid: "BV1LE411X74u",
+                time: 6
+            }, {
+                bvid: "BV1QE411Q7kk",
+                time: 91
+            }
+        ]
+    }, {
+        type: "music",
+        title: "我和我的祖国",
+        author: "王菲",
+        from: "我和我的祖国",
+        icon: "163/1392990601.jpg",
+        play: {
+            m163: "1392990601"
+        },
+        use: [
+            {
+                bvid: "BV1LE411X74u",
+                time: 31
+            }
+        ]
+    }, {
+        type: "music",
+        title: "爱河",
+        author: "蒋雪儿",
+        from: "房子",
+        icon: "163/1901261042.jpg",
+        play: {
+            m163: "1901261042"
+        },
+        use: [
+            {
+                bvid: "BV1LE411X74u",
+                time: 35
+            }
+        ]
+    }, {
+        type: "music",
+        title: "爱我中华",
+        author: "宋祖英",
+        from: "海姑娘",
+        icon: "163/1304695300.jpg",
+        play: {
+            m163: "1304695300"
+        },
+        use: [
+            {
+                bvid: "BV1LE411X74u",
+                time: 44
+            }
+        ]
+    }, {
+        type: "music",
+        title: "Roundabout",
+        author: "Yes",
+        from: "Yesstory",
+        icon: "163/20789751.jpg",
+        play: {
+            m163: "20789751"
+        },
+        use: [
+            {
+                bvid: "BV1LE411X74u",
+                time: 159
+            }, {
+                bvid: "BV1WJ411g7NP",
+                time: 18
+            }
+        ]
+    }, {
+        type: "music",
+        title: "千本桜",
+        author: "初音ミク / 黒うさP",
+        from: "千本桜",
+        icon: "163/26096272.jpg",
+        play: {
+            m163: "26096272"
+        },
+        use: [
+            {
+                bvid: "BV1QE411Q7kk",
+                time: 15
+            }
+        ]
+    }, {
+        type: "music",
+        title: "【五五开】穷开挂",
+        author: "茶几君梦二",
+        icon: "bili/18193325.jpg",
+        play: {
+            bili: {
+                bvid: "BV1FW411q7Js"
+            }
+        },
+        use: [
+            {
+                bvid: "BV1QE411Q7kk",
+                time: 36
+            }
+        ]
+    }, {
+        type: "music",
+        title: "【8848】穷开心",
+        author: "潮汕好男人",
+        icon: "bili/21535650.jpg",
+        play: {
+            bili: {
+                bvid: "BV14W411T7WJ",
+                pid: 1
+            }
+        },
+        use: [
+            {
+                bvid: "BV1QE411Q7kk",
+                time: 42
+            }
+        ]
+    }, {
+        type: "music",
+        title: "アカリがやってきたぞっ",
+        author: "紲星あかり / GYARI",
+        from: "超銀河キズユカ歌合戦",
+        icon: "163/1309901145.jpg",
+        play: {
+            m163: "1309901145",
+            bili: {
+                bvid: "BV1bW411r7XU"
+            }
+        },
+        use: [
+            {
+                bvid: "BV1QE411Q7kk",
+                time: 81
+            }
+        ]
+    },
+    template[0],
+    {
+        type: "music",
+        title: "「名探偵コナン」~メインテーマ",
+        author: "大野克夫",
+        from: "「名探偵コナン」メインテーマ",
+        icon: "163/27570832.jpg",
+        play: {
+            m163: "27570832"
+        },
+        use: [
+            {
+                bvid: "BV1WJ411g7NP",
+                time: 2
+            }
+        ]
+    }, {
+        type: "music",
+        title: "短兵相接",
+        author: "塞壬唱片-MSR / BaoUner",
+        from: "明日方舟OST1",
+        icon: "163/1371760674.jpg",
+        play: {
+            m163: "1371760676"
+        },
+        use: [
+            {
+                bvid: "BV1WJ411g7NP",
+                time: 36
+            }
+        ]
+    }
+]
